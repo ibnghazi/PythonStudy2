5dddb0c9e4c207324149761ad727cf56d0732b4e
ibnghazi <ibnghazi77@gmail.com>
2025-09-09 17:56:08 +0900

    1일차

---
+++ practice5/main.py
@@ -0,0 +1,27 @@
+
+
+while True:
+    print("*====== KH MINI GAME ======*")
+    print("\t1. 숫자 맞추기")
+    print("\t2. 가위바위보")
+    print("\t3. 주사위 게임")
+    print("\t0. 종료")
+
+    print()
+
+    menu = input("게임 선택: ")
+
+    if menu == "1":
+        # 숫자 맞추기 게임 실행
+        pass
+    elif menu == "2":
+        # 가위바위보 게임 실행
+        pass
+    elif menu == "3":
+        # 주사위게임 실행
+        pass
+    elif menu == "0":
+        print("*======== GAME END ========*")
+        break
+    else:
+        print("잘못 입력했습니다. 다시 입력해주세요.")
---
+++ practice5/number_game.py
@@ -0,0 +1,11 @@
+#practice5/mini_games/number_game.py
+
+'''
+1. 숫자 맞추기 게임
+   - number_game.py 모듈
+   - 함수명: play_number_game()
+   - 기능:
+       * 1~10 사이 랜덤 숫자를 생성
+       * 사용자 입력을 받아 숫자를 맞추면 "정답!" 출력
+       * 틀리면 "틀렸습니다. 정답은 X였습니다." 출력
+'''
